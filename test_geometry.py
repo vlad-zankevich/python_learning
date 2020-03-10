d55772c3fce1f9fbe05923e45cb61aa888a20c90
Alexey Zankevich <alexey.zankevich@onepeloton.com>
2020-03-10 16:19:44 -0400

    Added square tests

--- test_geometry.py
+++ test_geometry.py
@@ -1,4 +1,16 @@
-__author__ = 'vlad'
+from geometry import Square
 
 
+def test_square_constructor():
+	s = Square(1, 5, 2, 12)
 
+	assert s.x == 1
+	assert s.y == 5
+	assert s.a == 2
+	assert s.angle == 12
+
+
+def test_square_area():
+	s = Square(1, 5, 2, 12)
+
+	assert s.area() == 4
